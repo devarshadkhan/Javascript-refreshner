87f412eb6fcc7f406d87739d60e28f2911c1d86e
TechArshadKhan <122626113+TechArshadKhan@users.noreply.github.com>
2023-03-31 01:27:41 +0530

    JS variables and hoisting

--- Variables/Variables.js
+++ Variables/Variables.js
@@ -8,14 +8,14 @@
 // Const is Local Keyword this is not re-assign value and Block Scope.
 
 
-// var a = 10;
-// var a = 20  // first wale ko memory se clean kardega. var sahi tarika nhi used karne project me same variable ko 2 times decare kare hai or yeh value ko change bhi kar deta hai.
-// console.log(a)
+var a = 10;
+var a = 20  // first wale ko memory se clean kardega. var sahi tarika nhi used karne project me same variable ko 2 times decare kare hai or yeh value ko change bhi kar deta hai.
+console.log(a)
 
-// let b = 100;
-// // let b = 10 // 🥵 let ko same name defined nhi kar sakte hai. or yeh value ko re-assing bhi kar deta hai
-// console.log(b);
+let b = 100;
+// let b = 10 // 🥵 let ko same name defined nhi kar sakte hai. or yeh value ko re-assing bhi kar deta hai
+console.log(b);
 
-// const c = 10;
-// c = 20  // 🥵 same name defined nhi kar sakte hai. or yeh value ko re-assing nhi karta yeh fixed value rehti hai.
-// console.log(c);
+const c = 10;
+c = 20  // 🥵 same name defined nhi kar sakte hai. or yeh value ko re-assing nhi karta yeh fixed value rehti hai.
+console.log(c);
